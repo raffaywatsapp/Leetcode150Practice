09c3d0842dc8b3224506a80b0674cb606a3ecdc6
awaisbitcoin <awaism3277@gmail.com>
2025-08-26 16:36:23 +0500

    Solved LeetCode 27 Remove Element

---
+++ Array/27_Remove_Element.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        k = 0  # pointer for new length
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+
+if __name__ == "__main__":
+    # Example input
+    nums = [3, 2, 2, 3]
+    val = 3
+
+    sol = Solution()
+    k = sol.removeElement(nums, val)
+
+    print("New length:", k)
+    print("Array after removal:", nums[:k])
